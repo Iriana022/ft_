b64623d9428a859679fa40b4684033c0fef7cdbd
irazafim <irazafim@c1r3s6.42antananarivo.mg>
2026-02-05 10:16:31 +0300

    add DaisyUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,27 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="p-10 flex flex-col gap-4 items-center justify-center min-h-screen bg-base-200">
+      <h1 className="text-3xl font-bold text-primary">Test DaisyUI</h1>
+      <button className="btn btn-primary">Bouton Primaire</button>
+      <button className="btn btn-secondary">Bouton Secondaire</button>
+      <button className="btn btn-accent">Bouton Accent</button>
+      
+      <div className="alert alert-success shadow-lg">
+        <div>
+          <span>Succès ! Si tu vois ça en vert, Tailwind fonctionne.</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-				<p>Hello World!</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
