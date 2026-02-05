b7f09253af307d5b215e93de7f79089d5a1ddd6a
Pierrot Mihangy <pmihangy@c1r4s14.42antananarivo.mg>
2026-02-05 11:28:45 +0300

    integrate login design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="p-10 flex flex-col gap-4 items-center justify-center min-h-screen bg-base-200">
-      <h1 className="text-3xl font-bold text-primary">Test DaisyUI</h1>
-      <button className="btn btn-primary">Bouton Primaire</button>
-      <button className="btn btn-secondary">Bouton Secondaire</button>
-      <button className="btn btn-accent">Bouton Accent</button>
-      
-      <div className="alert alert-success shadow-lg">
-        <div>
-          <span>Succès ! Si tu vois ça en vert, Tailwind fonctionne.</span>
-        </div>
-      </div>
-    </div>
+			<Login/>
     </>
   )
 }
---
+++ frontend/src/components/Client.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Client() {
+  return <h1>Client</h1>;
+}
