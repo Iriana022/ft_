85d85f67d06b80a3177c2531da45d9587fd6f860
Pierrot Mihangy <pmihangy@c1r2s3.42antananarivo.mg>
2026-03-04 13:00:58 +0300

    fix(recharts): reinstall recharts after accidently remove it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,7 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css'
-import Login from './components/Login'
-import ProtectedRoute from './components/ProtectedRoute'
-import PublicRoute from './components/PublicRoute'
-import { DashboardLayout } from './components/DashboardLayout'
-import { ThemeProvider } from './context/ThemeContext'
-import Register from './components/Register'
-import ClientView from './views/ClientView';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <PublicRoute><Login/></PublicRoute>
-    },
-    {
-        path: '/login',
-        element: <PublicRoute><Login/></PublicRoute>
-    },
-    {
-        path: '/register',
-        element: <PublicRoute><Register /></PublicRoute>
-    },
-    {
-        path: '/dashboard',
-        element: (
-            <ProtectedRoute>
-                <DashboardLayout />
-            </ProtectedRoute>
-        )
-    }
-])
+import {ThemeProvider} from './context/ThemeContext'
+import {router} from './routes';
 
 function App() {
 	return (
