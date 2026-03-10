798a8357537948c83f2969ee10c192ee9fe6ec93
Pierrot Mihangy <pmihangy@c1r3s4.42antananarivo.mg>
2026-03-10 16:58:18 +0300

    feat(TicketInformation): implement TicketInformation component and show it when the user click on a specific ticket

--- frontend/src/components/client_components/Ticket.tsx
+++ frontend/src/components/client_components/Ticket.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import {useState} from 'react';
 import {type TicketType} from '../../types';
 import TicketStatusIndicator from './TicketStatusIndicator';
+import TicketInformation from './TicketInformation';
 
-function Ticket(props: TicketType) {
+interface TicketProps {
+	ticket: TicketType,
+}
+
+function Ticket(props: TicketProps) {
+	const [isOpen, setIsOpen] = useState(false);
 	const truncatedTitle =
-		props.title.length > 25
-			? `${props.title.substring(0, 25)}...`
-			: props.title;
+		props.ticket.title.length > 25
+			? `${props.ticket.title.substring(0, 25)}...`
+			: props.ticket.title;
 
 	return (
-		<div className="relative h-[130px] p-3 bg-blue-100 transition hover:bg-blue-200 rounded-md shadow-md cursor-pointer">
-			<h3 className="font-semibold text-base mb-2">{truncatedTitle}</h3>
-			<p className="text-sm line-clamp-2">{props.description}</p>
-			<div className="absolute right-2 bottom-2">
-				<TicketStatusIndicator status={props.status} />
+		<>
+			{/* Hidden by default */}
+			<TicketInformation
+				isOpen={isOpen}
+				onClose={() => setIsOpen(false)}
+				ticket={props.ticket}
+			/>
+			<div
+				className="relative h-[130px] p-3 bg-blue-100 transition hover:bg-blue-200 rounded-md shadow-md cursor-pointer"
+				onClick={() => setIsOpen(true)}
+			>
+				<h3 className="font-semibold text-base mb-2">{truncatedTitle}</h3>
+				<p className="text-sm line-clamp-2">{props.ticket.description}</p>
+				<div className="absolute right-2 bottom-2">
+					<TicketStatusIndicator status={props.ticket.status} />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
